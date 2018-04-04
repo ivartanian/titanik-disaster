0c8d743026fef90eb02ca5dd14880b8c994ea628
super <igor.vartanian@gmail.com>
2018-04-05 00:30:43 +0300

    Exploring Titanic Dataset

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,28 +1,14 @@
-# -*- coding: utf-8 -*-
-import os
-import click
-import logging
+import pandas as pd
 
+pd.set_option('expand_frame_repr', False)
+pd.set_option('colheader_justify', 'center')
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
-
-    # not used in this stub but often useful for finding various files
-    project_dir = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-
-    main()
+train = pd.read_csv("train.csv")
+print("-------------------------HEAD----------------------------------")
+print(train.head())
+print("-------------------------SHAPE----------------------------------")
+print(train.shape)
+print("-------------------------COLUMNS----------------------------------")
+print(train.columns)
+print("-------------------------COLUMN NAMES AND TYPES----------------------------------")
+print(train.dtypes)
